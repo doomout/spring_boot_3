0dd01c870910a8362fb1b66ee6dfb2c9c768419d
khg <winquake3@hanmail.net>
2024-08-29 21:31:02 +0900

    5강 셀프 체크에 회원 조회, 전체 조회 기능 추가

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,7 +6,9 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 이 클래스가 Spring MVC의 Controller임을 나타냅니다.
@@ -24,16 +26,26 @@ public class MemberController {
     @PostMapping("/join") // "/join" URL로 POST 요청이 오면 이 메서드가 실행됩니다.
     public String join(MemberForm memberForm) {
         log.info(memberForm.toString());
-
         // MemberForm 객체를 Member 엔티티로 변환합니다.
         Member member = memberForm.toEntity();
-
         log.info(member.toString());
-
         // Member 엔티티를 데이터베이스에 저장하고, 저장된 엔티티를 반환받습니다.
         Member saved = memberRepository.save(member);
-
         log.info(saved.toString());
         return "";
+    }
+    //특정 회원 조회
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
+    //전체 회원 조회
+    @GetMapping("/members")
+    public String index(Model model) {
+        Iterable<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members/index";
     }
 }
