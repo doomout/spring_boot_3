ddaeab93b621173a7011ba14d13efeacf211f8ff
khg <winquake3@hanmail.net>
2024-08-31 22:21:28 +0900

    셀프 체크 : 코드 간소화

--- src/main/java/com/example/firstproject/entity/Member.java
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity // 이 클래스가 JPA 엔티티임을 나타냅니다.
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
 public class Member {
     @Id // 이 필드가 엔티티의 기본 키임을 나타냅니다.
     @GeneratedValue // 기본 키 값을 자동으로 생성해줍니다
@@ -16,19 +24,4 @@ public class Member {
 
     @Column // 이 필드가 데이터베이스 테이블의 열과 매핑됨을 나타냅니다.
     String password;
-
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
