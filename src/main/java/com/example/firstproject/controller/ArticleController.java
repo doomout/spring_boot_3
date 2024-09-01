358f700b7d4e0d9ac9316ea621ae1e5da65f14e6
khg <winquake3@hanmail.net>
2024-09-01 23:17:17 +0900

    Edit 요청 받아 데이터 가져오기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -56,4 +56,13 @@ public class ArticleController {
         //3. 사용자에게 보여줄 뷰 페이지 설정하기
         return "articles/index";
     }
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) { //id를 매개변수로 받아오기
+        //수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null); //DB에서 수정할 데이터 가져오기
+        //모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        //뷰 페이지 설정하기
+        return "articles/edit";
+    }
 }
