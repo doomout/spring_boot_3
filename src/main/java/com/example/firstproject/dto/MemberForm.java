039cd989d561e9accec9af512ea38cbf6c6e160b
khg <winquake3@hanmail.net>
2024-09-02 11:34:48 +0900

    수정 기능 추가

--- src/main/java/com/example/firstproject/dto/MemberForm.java
+++ src/main/java/com/example/firstproject/dto/MemberForm.java
@@ -7,12 +7,12 @@ import com.example.firstproject.entity.Member;
 @AllArgsConstructor //생성자 단축
 @ToString //toString 단축
 public class MemberForm {
-    // 사용자의 이메일과 비밀번호를 저장하는 필드
+    private Long id;
     private String email;
     private String password;
 
     // MemberForm 객체를 Member 엔티티로 변환하는 메서드
     public Member toEntity() {
-        return new Member(null, email, password);
+        return new Member(id, email, password);
     }
 }
