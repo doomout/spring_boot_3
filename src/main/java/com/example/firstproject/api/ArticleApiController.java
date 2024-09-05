889887349aab122c44d6a9ac75d5a0fbd8f86ff6
khg <winquake3@hanmail.net>
2024-09-06 01:20:02 +0900

    REST API: Patch 구현

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,16 +3,20 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class ArticleApiController {
     @Autowired //게시글 리파지터리 주입
     private ArticleRepository articleRepository;
-    //GET
+    //GET(읽기)
     @GetMapping("/api/articles")
     public List<Article> index() {
         return articleRepository.findAll();
@@ -21,12 +25,29 @@ public class ArticleApiController {
     public Article show(@PathVariable Long id) {
         return articleRepository.findById(id).orElse(null);
     }
-    //POST
+    //POST(생성)
     @PostMapping("/api/articles")
     public Article create(@RequestBody ArticleForm dto) { //@RequestBody:요청시 body에 실어 보내는 데이터를 메개변수로 받아올 수 있다.
         Article article = dto.toEntity();
         return articleRepository.save(article);
     }
-    //PATCH
+    //PATCH(수정)
+    @PatchMapping("/api/articles/{id}")
+    //ResponseEntity<> 클라이언트 요청 오류를 반환하기 위해 사용
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+        //1.DTO -> 엔티티 변환하기
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        //2.타딧 조회하기
+        Article target = articleRepository.findById(id).orElse(null);
+        //3.잘못된 요청 처리하기
+        if(target == null || id != article.getId()) {
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null); //잘못된 응답
+        }
+        //4.업데이트 및 정상 응답(200)하기
+        Article updated = articleRepository.save(article); //article 엔티티 DB에 저장
+        return ResponseEntity.status(HttpStatus.OK).body(updated); //정상 응답 
+    }
     //DELETE
 }
