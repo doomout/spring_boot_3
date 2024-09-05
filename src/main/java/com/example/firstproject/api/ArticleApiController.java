d1a1d8565c4d24dbac70dbb931473ea0b90d1f8f
khg <winquake3@hanmail.net>
2024-09-05 20:29:12 +0900

    REST API:GET 구현

---
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -0,0 +1,28 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ArticleApiController {
+    @Autowired //게시글 리파지터리 주입
+    private ArticleRepository articleRepository;
+    //GET
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+    //POST
+    //PATCH
+    //DELETE
+}
