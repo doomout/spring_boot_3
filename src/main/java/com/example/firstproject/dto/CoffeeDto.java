0e2202de65f11804f80913261e4823e69810055f
khg <winquake3@hanmail.net>
2024-09-07 19:53:45 +0900

    DTO, Entity 추가 구현

--- src/main/java/com/example/firstproject/dto/CoffeeDto.java
+++ src/main/java/com/example/firstproject/dto/CoffeeDto.java
@@ -1,10 +1,17 @@
 package com.example.firstproject.dto;
 
+import com.example.firstproject.entity.Coffee;
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
+@ToString
 public class CoffeeDto {
     private Long id; 
     private String name; //커피 이름
     private String price; //가격
+
+    public Coffee toEntity() {
+        return new Coffee(id, name, price);
+    }
 }
--- src/main/java/com/example/firstproject/entity/Coffee.java
+++ src/main/java/com/example/firstproject/entity/Coffee.java
@@ -1,16 +1,13 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 public class Coffee {
     @Id //이 필드가 엔티티의 기본키
-    @GeneratedValue //기본키의 값을 자동으로 생성
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키의 값을 자동으로 생성
     Long id;
 
     @Column
@@ -18,4 +15,11 @@ public class Coffee {
 
     @Column
     String price;
+
+    public void patch(Coffee coffee) {
+        if(coffee.name != null)
+            this.name = coffee.name;
+        if(coffee.price != null)
+            this.price = coffee.price;
+    }
 }
