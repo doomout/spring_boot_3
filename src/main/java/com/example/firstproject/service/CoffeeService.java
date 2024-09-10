f9b76e0703d13e291b1f0d91d5feb5bd69a7c2a6
khg <winquake3@hanmail.net>
2024-09-10 15:14:38 +0900

    Self_Check : 전체 조회, id 조회 리팩토링

---
+++ src/main/java/com/example/firstproject/service/CoffeeService.java
@@ -0,0 +1,24 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.entity.Coffee;
+import com.example.firstproject.repository.CoffeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CoffeeService {
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+
+    public Iterable<Coffee> index() {
+        return coffeeRepository.findAll();
+    }
+
+    public Coffee show(Long id) {
+        return coffeeRepository.findById(id).orElse(null);
+    }
+
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO article(id, title, content) VALUES(1, '가가가가가', '1111');
 INSERT INTO article(id, title, content) VALUES(2, '나나나나나', '2222');
 INSERT INTO article(id, title, content) VALUES(3, '다다다다다', '3333');
+
+
+INSERT INTO coffee(id, name, price) VALUES(1, '아메리카노', '5000');
+INSERT INTO coffee(id, name, price) VALUES(2, '라떼', '6000');
+INSERT INTO coffee(id, name, price) VALUES(3, '콜드브루', '6500');
