d24f7477bec31093fe2af79f8db31f05bb02ac87
khg <winquake3@hanmail.net>
2024-09-16 20:43:08 +0900

    댓글 삭제 기능 추가

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -44,7 +44,7 @@ public class CommentService {
         //1.게시글 조회 및 예외 발생
         Article article = articleRepository
                 .findById(articleId)
-                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! " + "대상 게시글이 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! 대상 게시글이 없습니다."));
         //2.댓글 엔티티 생성
         Comment comment = Comment.createComment(dto, article);
         //3.댓글 엔티티를 DB에 저장
@@ -57,12 +57,23 @@ public class CommentService {
     public CommentDto update(Long id, CommentDto dto) {
         //1.댓글 조회 및 예외 발생
         Comment target = commentRepository.findById(id) //수정할 댓글 가져오기
-                .orElseThrow(() -> new IllegalArgumentException("댓글 수정 실패!" + "대상 댓글이 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("댓글 수정 실패! 대상 댓글이 없습니다."));
         //2.댓글 수정
         target.patch(dto);
         //3.DB로 갱신
         Comment updated = commentRepository.save(target);
         //4.댓글 엔티티를 DTO로 변환 및 반환
         return CommentDto.createCommentDto(updated);
+    }
+
+    @Transactional
+    public CommentDto delete(Long id) {
+        //1.댓글 조회 및 예외 발생
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 삭제 실패! 대상이 없습니다."));
+        //2.댓글 삭제
+        commentRepository.delete(target);
+        //3.삭제 댓글을 DTO로 변환 및 반환
+        return CommentDto.createCommentDto(target);
     }
 }
